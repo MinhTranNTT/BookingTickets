c3e6f3207f714f8b241f32bcfe15c09d72623550
MinhTranNTT <tvhuuminhntt@gmail.com>
2022-11-18 00:00:12 +0700

    init project

---
+++ Booking/BookingWeb/BookingBackEnd/src/main/java/com/booking/admin/BookingBackEndApplication.java
@@ -0,0 +1,16 @@
+package com.booking.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBackEndApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBackEndApplication.class, args);
+	}
+	
+//	public static void main(String[] args) {
+//		System.out.println("HAA");
+//	}
+}
