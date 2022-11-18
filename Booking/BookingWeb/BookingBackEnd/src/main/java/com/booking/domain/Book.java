d126ca591c4d4b8d51b92adfc6f9a9316efc189a
tvhminh <tvhminh@hanatourjsv.com>
2022-11-18 16:52:38 +0700

    add files

---
+++ Booking/BookingWeb/BookingBackEnd/src/main/java/com/booking/domain/Book.java
@@ -0,0 +1,14 @@
+package com.booking.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+	
+	private Integer id;
+	private String bookName;
+	private String orderCode;
+	
+}
